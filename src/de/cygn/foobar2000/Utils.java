67672dbbeae3249b6125f44a5ca0586ae18b1734
Thomas Friedel <thomas.friedel@berlin>
2012-09-11 01:54:13 +0200

    strings are now treated as utf-8

--- src/de/cygn/foobar2000/Utils.java
+++ src/de/cygn/foobar2000/Utils.java
@@ -3,9 +3,12 @@ package de.cygn.foobar2000;
 import java.io.IOException;
 import java.nio.BufferUnderflowException;
 import java.nio.ByteBuffer;
+import java.util.ArrayList;
 
 public class Utils {
-	static int MAX_STRING_LENGTH=1000000;
+
+	static int MAX_STRING_LENGTH = 1000000;
+
 	public static String readStringAtPointer(ByteBuffer bb, int string_table_offset) throws IOException {
 		int offset = bb.getInt();
 		int old_position = bb.position();
@@ -15,11 +18,13 @@ public class Utils {
 		return s;
 	}
 
-	/** true if string empty or null */
+	/**
+	 * true if string empty or null
+	 */
 	public static boolean isEmpty(String str) {
 		return str == null || str.length() == 0;
 	}
-	
+
 	public static String cleanFoldername(String foldername) {
 		if (foldername.startsWith("file://")) {
 			foldername = foldername.substring(7);
@@ -30,11 +35,12 @@ public class Utils {
 	/* first reads length of string from ByteBuffer, then reads string of that length */
 	public static String getString(ByteBuffer bb) throws IOException {
 		int len = bb.getInt();
-		if (len>MAX_STRING_LENGTH)
-			throw new IOException(String.format("string length too big: %d. Position: %d",len,bb.position()-4));
+		if (len > MAX_STRING_LENGTH) {
+			throw new IOException(String.format("string length too big: %d. Position: %d", len, bb.position() - 4));
+		}
 		byte[] b = new byte[len];
 		bb.get(b, 0, len);
-		String result = new String(b);
+		String result = new String(b, "UTF-8");
 		return result;
 	}
 
@@ -47,15 +53,39 @@ public class Utils {
 	public static long fileTimeToMillis(long fileTime) {
 		return (fileTime / 10000L) - 11644473600000L;
 	}
-	
+
+	public static String formatIntoHHMMSS(int secsIn) {
+		int hours = secsIn / 3600;
+		int remainder = secsIn % 3600;
+		int minutes = remainder / 60;
+		int seconds = remainder % 60;
+		String result = "";
+		if (hours>0)
+			result = String.valueOf(hours) + ":";
+		if (hours==0)
+			result = String.valueOf(minutes) + ":";
+		else
+			result += ((minutes < 10 ? "0" : "") + minutes + ":");
+		if (result.length()==0)
+			result = String.valueOf(seconds);
+		else
+			result += ((seconds < 10 ? "0" : "") + seconds);
+		return result;
+	}
+
 	public static String readNullTerminatedString(ByteBuffer bb) throws IOException {
-		StringBuilder sb = new StringBuilder(32);
-		while (bb.hasRemaining()) // safer  
-		{  
-			char c = (char)bb.get();  
-			if (c == '\0') break;  
-			sb.append(c);  
-		}  
-		return sb.toString();
+		int start = bb.position();
+		int count = 0;
+		while (bb.get() != 0) {
+			count++;
+		}
+		int current_position = bb.position();
+		int len = Math.min(count, MAX_STRING_LENGTH);
+		byte[] b = new byte[len];
+		bb.position(start);
+		bb.get(b);
+		bb.position(current_position);
+		String result = new String(b, "UTF-8");
+		return result;
 	}
 }
