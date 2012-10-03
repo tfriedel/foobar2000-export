334c7b12d9738636d5e1b35797530cfc204bc70b
Thomas Friedel <thomas.friedel@berlin>
2012-10-04 01:21:16 +0200

    test function which is run by H2

---
+++ src/de/cygn/foobar2000/h2/NewClass.java
@@ -0,0 +1,18 @@
+/*
+ */
+package de.cygn.foobar2000.h2;
+
+/**
+ *
+ * @author Thomas Friedel
+ */
+public class NewClass {
+
+	public static String testFunc(String value) {
+		if (value == null) {
+			return "empty2";
+		} else {
+			return value;
+		}
+	}
+}
