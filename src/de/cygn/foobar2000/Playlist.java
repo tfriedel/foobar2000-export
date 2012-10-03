bc3952b62b0ec7076a1ea2e515fdd7f5a36a652d
Thomas Friedel <thomas.friedel@berlin>
2012-10-04 01:19:05 +0200

    added id field

--- src/de/cygn/foobar2000/Playlist.java
+++ src/de/cygn/foobar2000/Playlist.java
@@ -12,10 +12,13 @@ import java.util.logging.Logger;
  * @author Thomas Friedel
  */
 public class Playlist {
+
 	public enum PlaylistType {NORMAL, AUTO};
 	String filename;
 	String title;
 	PlaylistType type = PlaylistType.NORMAL;
+	int id;
+	
 	private ArrayList<Track> tracks;
 	
 	public ArrayList<Track> getTracks() {
@@ -37,7 +40,12 @@ public class Playlist {
 	public void setTracks(ArrayList<Track> tracks) {
 		this.tracks = tracks;
 	}
-	
+
+	public int getNr() {
+			String playlist_filename = new File(filename).getName();
+			return Integer.valueOf(playlist_filename.substring(0, playlist_filename.length() - 4));
+	}
+
 	@Override
 	public String toString() {
 		String result = title;
